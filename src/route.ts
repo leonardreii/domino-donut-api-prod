a3310c238202c5580d36ec672982bf16686209df
Neullson <melsasail_neullson@yahoo.com>
2017-01-27 14:09:41 +0700

    Initial Order Controller
    Initial Order Controller

--- src/route.ts
+++ src/route.ts
@@ -2,6 +2,7 @@ import {Token} from './services/token.service';
 
 import { DriverController } from './controller/driver/driver.controller';
 import { CustomerController } from './controller/customer/customer.controller';
+import { OrderController } from './controller/order/order.controller';
 
 export function Routing(router){
     const tokenService: Token = new Token();
@@ -11,4 +12,7 @@ export function Routing(router){
 
     const customerController:CustomerController = new CustomerController();
     router.post('/customer/finddriver',customerController.findDriver);
+
+    const orderCtrl:OrderController = new OrderController();
+    router.put('/order/cancelorder',orderCtrl.CancelOrder);
 }
