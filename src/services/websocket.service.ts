f0dbc445d6af19acc3ba659368674f9ed24682a3
rickyrk <rickyrk11@gmail.com>
2017-02-22 14:16:55 +0700

    Commit

--- src/services/websocket.service.ts
+++ src/services/websocket.service.ts
@@ -8,10 +8,20 @@ export class WebSocketService {
         WebSocketService.sockets = {};
 	}
     Listen(port:any){
+        var self = this;
         this.http.listen(port);
         this.io.on('connection', function (socket:any) {
-            WebSocketService.sockets[socket.id]=socket;
+        WebSocketService.sockets[socket.id]=socket;
         socket.emit('socketId', { socketId: socket.id });
+        socket.on('send-order-detail', (ordersdetail:any) => {
+            self.io.emit('placeId', {type:'ordersdetail', text: ordersdetail});
+            console.log( ordersdetail);
+        });
+        console.log('user connected');
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+        });
+
         // socket.on('my other event', function (data:any) {
         //         console.log(`socket id : ${socket.id} ,data :${data}`);
         //         this.io.sockets.connected[socket.id].emit('news', `${data} gile lu ndro`);
@@ -19,3 +29,4 @@ export class WebSocketService {
         });
     }
 }
+
